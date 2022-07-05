306a5f004e78b7f49a3a611a926f32cae2f32d09
hong-kyungs <hkyungs11@naver.com>
2022-07-06 00:08:42 +0900

    useRef 가상돔 참조하기

--- src/App.js
+++ src/App.js
@@ -1,6 +1,38 @@
+import './scss/style.scss';
+import { useRef, useState } from 'react';
+
+const boxStyle = {
+	width: 200,
+	height: 200,
+	backgroundColor: 'aqua',
+	marginTop: 50,
+	transitionDuration: '0.5s',
+};
 function App() {
+	const box = useRef(null);
+	// console.log(box);
+	const deg = 45;
+	let [Num, setNum] = useState(0);
+
 	return (
-		<h1>Hello</h1>
+		<section className='wrap' style={{ padding: 50 }}>
+			<button
+				onClick={() => {
+					setNum(--Num);
+					box.current.style.transform = `rotate(${deg * Num}deg)`;
+				}}>
+				prev
+			</button>
+			<button
+				onClick={() => {
+					setNum(++Num);
+					box.current.style.transform = `rotate(${deg * Num}deg)`;
+				}}>
+				next
+			</button>
+
+			<article style={boxStyle} ref={box}></article>
+		</section>
 	);
 }
 
